f32154edaf50f8a7aeb5b860a2c7d0b3da7b5079
Mshaninah <shaninah.mohammad@gmail.com>
2022-03-25 10:24:10 +0100

    Config sync

--- web/modules/custom/employee_overview/src/EventSubscriber/EmployeeOverviewSubscriber.php
+++ web/modules/custom/employee_overview/src/EventSubscriber/EmployeeOverviewSubscriber.php
@@ -7,9 +7,6 @@ use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\employee_overview\Event\EmployeeOverviewEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Employee Overview event subscriber.
@@ -21,12 +18,7 @@ class EmployeeOverviewSubscriber implements EventSubscriberInterface {
    *
    * @var MessengerInterface
    */
-  protected $messenger;
-
-  /**
-   * @var DateFormatterInterface
-   */
-  private $date_formatter;
+  protected MessengerInterface $messenger;
 
   /**
    * Constructs event subscriber.
@@ -34,29 +26,8 @@ class EmployeeOverviewSubscriber implements EventSubscriberInterface {
    * @param MessengerInterface $messenger
    *   The messenger.
    */
-  public function __construct(MessengerInterface $messenger, DateFormatterInterface $date_formatter) {
+  public function __construct(MessengerInterface $messenger) {
     $this->messenger = $messenger;
-    $this->date_formatter = $date_formatter;
-  }
-
-  /**
-   * Kernel request event handler.
-   *
-   * @param GetResponseEvent $event
-   *   Response event.
-   */
-  public function onKernelRequest(GetResponseEvent $event) {
-    $this->messenger->addStatus(__FUNCTION__);
-  }
-
-  /**
-   * Kernel response event handler.
-   *
-   * @param FilterResponseEvent $event
-   *   Response event.
-   */
-  public function onKernelResponse(FilterResponseEvent $event) {
-    $this->messenger->addStatus(__FUNCTION__);
   }
 
   /**
@@ -67,25 +38,16 @@ class EmployeeOverviewSubscriber implements EventSubscriberInterface {
    */
 
   public function onUserLogin(EmployeeOverviewEvent $event) {
-    $last_logged_in = $this->date_formatter->format($event->account->getLastLoginTime(), 'short');
     $currentUserName = $event->account->getAccountName();
     $command = escapeshellcmd('python3 ./drupal.py');
     $output = shell_exec($command);
     $response_array = ['user_name' => $currentUserName];
     \Drupal::state()->set('User', $currentUserName);
-    if (empty($last_logged_in)) {
-      $last_logged_in = 'Never';
-    }
 
     $this->messenger
       ->addStatus($this->t('<strong>Hey there</strong>: %name.',
         [
           '%name' => $currentUserName,
-        ]
-      ))
-      ->addStatus($this->t('<strong>You last logged in</strong>: %last_logged_in',
-        [
-          '%last_logged_in' => $last_logged_in
         ]
       ));
   }
@@ -96,8 +58,6 @@ class EmployeeOverviewSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     return [
-      KernelEvents::REQUEST => ['onKernelRequest'],
-      KernelEvents::RESPONSE => ['onKernelResponse'],
       EmployeeOverviewEvent::EVENT_NAME => ['onUserLogin']
     ];
   }
