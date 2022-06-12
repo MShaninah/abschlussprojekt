1d5839a75539057ea1bbf909db6703df102dc785
Mohamad <shaninah@die-etagen.de>
2022-06-12 19:48:30 +0200

    Squat Übung hinzugefügt

--- web/track.py
+++ web/track.py
@@ -25,17 +25,20 @@ class VideoStream:
     _cap = cv2.VideoCapture(0)
 
     # Curl Counter
-    _counter = 0
+    _armCounter = 0
+    _legCounter = 0
     _stage = None
     _drupal_user = DrupalUser()
     """
-      a function to calculate the angle between Wrist, Elbow and Shoulder
+      a function to calculate the angle between three points:
+       for the first exercise the points are Wrist, Elbow and Shoulder
+       for the other one: Hip, Knee and Ankle
       """
 
     def calculate_angle(self, a, b, c):
-        a = np.array(a)  # First point "the Wrist"
-        b = np.array(b)  # Middle point "the Elbow"
-        c = np.array(c)  # End Point "the Shoulder"
+        a = np.array(a)  # First point
+        b = np.array(b)  # Middle point
+        c = np.array(c)  # End Point
 
         radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
         angle = np.abs(radians * 180.0 / np.pi)
@@ -50,6 +53,9 @@ class VideoStream:
     def track(self):
         # I'm using "with" keyword because I'm working with unmanaged resource (Webcam Stream).
         with self._mpPose.Pose(min_detection_confidence=0.9, min_tracking_confidence=0.9) as pose:
+            if self._cap is None or not self._cap.isOpened():
+                print('Warning: unable to open video source: ', 'Webcam')
+                return
             # As long as the Video Feed is open, this loop will read the feed from the Webcam and save it in the
             # variable "frame" as a video feed
             while self._cap.isOpened():
@@ -76,29 +82,86 @@ class VideoStream:
                     landmarks = results.pose_landmarks.landmark
 
                     # Get coordinates
-                    shoulder = [landmarks[self._mpPose.PoseLandmark.LEFT_SHOULDER.value].x,
-                                landmarks[self._mpPose.PoseLandmark.LEFT_SHOULDER.value].y]
-                    elbow = [landmarks[self._mpPose.PoseLandmark.LEFT_ELBOW.value].x,
-                             landmarks[self._mpPose.PoseLandmark.LEFT_ELBOW.value].y]
-                    wrist = [landmarks[self._mpPose.PoseLandmark.LEFT_WRIST.value].x,
-                             landmarks[self._mpPose.PoseLandmark.LEFT_WRIST.value].y]
+                    left_shoulder = [landmarks[self._mpPose.PoseLandmark.LEFT_SHOULDER.value].x,
+                                     landmarks[self._mpPose.PoseLandmark.LEFT_SHOULDER.value].y]
+
+                    left_elbow = [landmarks[self._mpPose.PoseLandmark.LEFT_ELBOW.value].x,
+                                  landmarks[self._mpPose.PoseLandmark.LEFT_ELBOW.value].y]
+
+                    left_wrist = [landmarks[self._mpPose.PoseLandmark.LEFT_WRIST.value].x,
+                                  landmarks[self._mpPose.PoseLandmark.LEFT_WRIST.value].y]
+
+                    right_shoulder = [landmarks[self._mpPose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                                      landmarks[self._mpPose.PoseLandmark.RIGHT_SHOULDER.value].y]
+
+                    right_elbow = [landmarks[self._mpPose.PoseLandmark.RIGHT_ELBOW.value].x,
+                                   landmarks[self._mpPose.PoseLandmark.RIGHT_ELBOW.value].y]
+
+                    right_wrist = [landmarks[self._mpPose.PoseLandmark.RIGHT_WRIST.value].x,
+                                   landmarks[self._mpPose.PoseLandmark.RIGHT_WRIST.value].y]
+
+                    left_hip = [landmarks[self._mpPose.PoseLandmark.LEFT_HIP.value].x,
+                                landmarks[self._mpPose.PoseLandmark.LEFT_HIP.value].y]
+
+                    left_knee = [landmarks[self._mpPose.PoseLandmark.LEFT_KNEE.value].x,
+                                 landmarks[self._mpPose.PoseLandmark.LEFT_KNEE.value].y]
+
+                    left_ankle = [landmarks[self._mpPose.PoseLandmark.LEFT_ANKLE.value].x,
+                                  landmarks[self._mpPose.PoseLandmark.LEFT_ANKLE.value].y]
+
+                    right_hip = [landmarks[self._mpPose.PoseLandmark.RIGHT_HIP.value].x,
+                                 landmarks[self._mpPose.PoseLandmark.RIGHT_HIP.value].y]
+
+                    right_knee = [landmarks[self._mpPose.PoseLandmark.RIGHT_KNEE.value].x,
+                                  landmarks[self._mpPose.PoseLandmark.RIGHT_KNEE.value].y]
+
+                    right_ankle = [landmarks[self._mpPose.PoseLandmark.RIGHT_ANKLE.value].x,
+                                   landmarks[self._mpPose.PoseLandmark.RIGHT_ANKLE.value].y]
 
                     # Calculate angle
-                    angle = self.calculate_angle(shoulder, elbow, wrist)
+                    left_arm_angle = self.calculate_angle(left_shoulder, left_elbow, left_wrist)
+                    right_arm_angle = self.calculate_angle(right_shoulder, right_elbow, right_wrist)
+                    right_leg_angle = self.calculate_angle(right_hip, right_knee, right_ankle)
+                    left_leg_angle = self.calculate_angle(left_hip, left_knee, left_ankle)
 
                     # Visualize angle
-                    cv2.putText(image, str(angle),
-                                tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                    cv2.putText(image, str(left_arm_angle),
+                                tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                                 )
 
                     # Curl Counter logic
-                    if angle > 160:
-                        stage = "down"
-                    if angle < 30 and stage == "down":
-                        stage = "up"
-                        self._counter += 1
-                        print(self._counter)
+                    if left_arm_angle > 160:
+                        left_arm_stage = "down"
+
+                    if left_arm_angle < 30 and left_arm_stage == "down":
+                        left_arm_stage = "up"
+                        self._armCounter += 1
+                        print(self._armCounter)
+
+                    if right_arm_angle > 160:
+                        right_arm_stage = "down"
+
+                    if right_arm_angle < 30 and right_arm_stage == "down":
+                        right_arm_stage = "up"
+                        self._armCounter += 1
+                        print(self._armCounter)
+
+                    if right_leg_angle > 160:
+                        right_leg_stage = "down"
+
+                    if right_leg_angle < 40 and right_leg_stage == "down":
+                        right_leg_stage = "up"
+                        self._legCounter += 1
+                        print("LegCounter",self._legCounter)
+
+                    if left_leg_angle > 160:
+                        left_leg_stage = "down"
+
+                    if left_leg_angle < 40 and left_leg_stage == "down":
+                        left_leg_stage = "up"
+                        self._legCounter += 1
+                        print("LegCounter", self._legCounter)
 
                 except:
                     pass
@@ -113,7 +176,7 @@ class VideoStream:
                 """ Representing Data """
                 cv2.putText(image, 'Represented Data', (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1,
                             cv2.LINE_AA)
-                cv2.putText(image, str(self._counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2,
+                cv2.putText(image, str(self._armCounter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2,
                             cv2.LINE_AA)
 
                 """ Render detection """
@@ -130,9 +193,7 @@ class VideoStream:
                 """ Clear the feed using the key 'q', or by closing the popup """
                 if cv2.waitKey(10) & 0xFF == ord('q') or cv2.getWindowProperty('Mediapipe Feed',
                                                                                cv2.WND_PROP_VISIBLE) < 1:
-                    self._drupal_user.write_entity(self._counter)
+                    self._drupal_user.write_entity(self._armCounter, self._legCounter)
                     self._cap.release()
                     cv2.destroyAllWindows()
                     break
-
-
